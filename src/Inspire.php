70e0373681e12f2eba77a6c7bf7d9add5eb8efad
Auriane <auriane@uccelolabs.com>
2022-02-22 10:43:05 +0100

    1st commit

---
+++ src/Inspire.php
@@ -0,0 +1,12 @@
+<?php
+ namespace Bref\Demo;
+
+ class Inspire {
+     public function __construct()
+     {
+         //
+     }
+     public function displayInspiration(){
+         return 'lol';
+     }
+ }
